e4f565f9b7311ef8c74c20f881e8fc73e1141406
Cyril David <cyx.ucron@gmail.com>
2010-10-05 02:21:40 +0800

    Vendor haml.

---
+++ vendor/gems/haml-3.0.21/vendor/sass/lib/sass/plugin/rails.rb
@@ -0,0 +1,41 @@
+unless defined?(Sass::RAILS_LOADED)
+  Sass::RAILS_LOADED = true
+
+  module Sass::Plugin::Configuration
+    # Different default options in a rails envirionment.
+    def default_options
+      @default_options ||= {
+        :always_update      => false,
+        :template_location => Sass::Util.rails_root + '/public/stylesheets/sass',
+        :css_location      => Sass::Util.rails_root + '/public/stylesheets',
+        :cache_location    => Sass::Util.rails_root + '/tmp/sass-cache',
+        :always_check      => Sass::Util.rails_env == "development",
+        :quiet             => Sass::Util.rails_env != "production",
+        :full_exception    => Sass::Util.rails_env != "production"
+      }.freeze
+    end
+  end
+
+  Sass::Plugin.options.reverse_merge!(Sass::Plugin.default_options)
+
+  if defined?(ActionController::Metal)
+    # Rails >= 3.0
+    require 'sass/plugin/rack'
+    Rails.configuration.middleware.use(Sass::Plugin::Rack)
+  elsif defined?(ActionController::Dispatcher) &&
+      defined?(ActionController::Dispatcher.middleware)
+    # Rails >= 2.3
+    require 'sass/plugin/rack'
+    ActionController::Dispatcher.middleware.use(Sass::Plugin::Rack)
+  else
+    module ActionController
+      class Base
+        alias_method :sass_old_process, :process
+        def process(*args)
+          Sass::Plugin.check_for_updates
+          sass_old_process(*args)
+        end
+      end
+    end
+  end
+end
