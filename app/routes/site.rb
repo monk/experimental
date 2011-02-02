7f3122fc52bb7029443ea692e4327bfd972466ec
Rico Sta. Cruz <rico@ambiescent.com>
2011-02-03 05:32:56 +0800

    Move the introductory notes to README, and remove references in it to Redis.

--- app/routes/site.rb
+++ app/routes/site.rb
@@ -1,9 +1,12 @@
 class Main
   get '/' do
-    @redis = {
-      :port => Ohm.redis.client.port,
-      :host => Ohm.redis.client.host
-    }
+    require 'maruku'
+    readme = Main.root('README.markdown')
+    text   = File.open(readme) { |f| f.read }
+    text   = text.force_encoding("UTF-8")
+    text   = text.match(/Welcome.*$/m)[0]  if text['Welcome']
+
+    @home_text = Maruku.new(text).to_html
 
     haml :home
   end
